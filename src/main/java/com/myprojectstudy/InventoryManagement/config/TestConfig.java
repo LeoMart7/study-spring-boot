f5502654c920bde23ff2e4538954ef8da8dc0b84
LeoMart7 <lbartiliano@hotmail.com>
2026-04-13 01:34:24 -0300

    enum OrderStatus

--- src/main/java/com/myprojectstudy/InventoryManagement/config/TestConfig.java
+++ src/main/java/com/myprojectstudy/InventoryManagement/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.myprojectstudy.InventoryManagement.entities.Order;
 import com.myprojectstudy.InventoryManagement.entities.User;
+import com.myprojectstudy.InventoryManagement.entities.enums.OrderStatus;
 import com.myprojectstudy.InventoryManagement.repositories.OrderRepository;
 import com.myprojectstudy.InventoryManagement.repositories.UserRepository;
 
@@ -30,12 +31,13 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Maria Madalena", "mairamada@gmail.com", "21974114516", "789456");
 		
 		
-		Order o1 = new Order(null, Instant.parse("2026-02-12T15:50:15Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2026-02-01T22:20:12Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2026-05-30T16:10:11Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2026-02-12T15:50:15Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2026-02-01T22:16:34Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2026-02-01T22:20:12Z"), OrderStatus.PAID, u2);
+		Order o4 = new Order(null, Instant.parse("2026-02-28T16:10:11Z"), OrderStatus.DELIVERED, u1);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
-		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
 		
 	} 
 	
